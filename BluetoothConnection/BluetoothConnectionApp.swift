22723997668e06a035d41722f5ea0b229c4ccfb9
VineeshHS <vineeshhs367@gmail.com>
2024-05-10 05:36:19 +0530

    Initial Commit

---
+++ BluetoothConnection/BluetoothConnectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothConnectionApp.swift
+//  BluetoothConnection
+//
+//  Created by ViNEESH HS on 10/05/24.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothConnectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
